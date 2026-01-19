90dae74cbb4cbcb4c46a42664d6b9987035fe44f
unknown <michellesaski363@gmail.com>
2026-01-19 16:30:29 +0800

    add admin authorization and user CRUD endpoints

--- admin/users.php
+++ admin/users.php
@@ -2,38 +2,38 @@
 require "../auth/middleware.php";
 require "../config/database.php";
 require "../controllers/UserController.php";
-
-// ADMIN ONLY
+ 
+// admin only
 if ($currentUser['role'] !== 'admin') {
-    jsonResponse(["message" => "Access denied"], 403);
+    jsonResponse(["message" => "Akses ditolak"], 403);
 }
-
+ 
 $db = (new Database())->connect();
 $controller = new UserController($db);
-
+ 
 $method = $_SERVER['REQUEST_METHOD'];
 $id = $_GET['id'] ?? null;
 $data = json_decode(file_get_contents("php://input"), true);
-
+ 
 switch ($method) {
     case "GET":
         $id ? $controller->show($id) : $controller->index();
         break;
-
+ 
     case "POST":
         $controller->store($data);
         break;
-
+ 
     case "PUT":
         isset($data['password'])
             ? $controller->updatePassword($id, $data)
             : $controller->update($id, $data);
         break;
-
+ 
     case "DELETE":
         $controller->destroy($id);
         break;
-
+ 
     default:
-        jsonResponse(["message" => "Method not allowed"], 405);
+        jsonResponse(["message" => "Method tidak diizinkan"], 405);
 }
