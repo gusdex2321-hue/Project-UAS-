dcffcebf7fee52f8ca53ab9ed4863ca073b7dbf1
unknown <gusdex2321@gmail.com>
2026-01-21 03:04:03 +0800

    revisi_dikit

--- admin/users.php
+++ admin/users.php
@@ -1,39 +1,52 @@
 <?php
-require "../auth/middleware.php";
-require "../config/database.php";
-require "../controllers/UserController.php";
- 
+require_once "../auth/middleware.php";
+require_once "../config/database.php";
+require_once "../controllers/UserController.php";
+require_once "../helpers/response.php";
 
 if ($currentUser['role'] !== 'admin') {
     jsonResponse(["message" => "Akses ditolak"], 403);
 }
- 
+
 $db = (new Database())->connect();
 $controller = new UserController($db);
- 
+
 $method = $_SERVER['REQUEST_METHOD'];
 $id = $_GET['id'] ?? null;
 $data = json_decode(file_get_contents("php://input"), true);
- 
+
 switch ($method) {
+
     case "GET":
         $id ? $controller->show($id) : $controller->index();
         break;
- 
+
     case "POST":
         $controller->store($data);
         break;
- 
+
     case "PUT":
-        isset($data['password'])
-            ? $controller->updatePassword($id, $data)
-            : $controller->update($id, $data);
+        if (!$id) {
+            jsonResponse(["message" => "ID user wajib diisi"], 422);
+        }
+
+        // update username & role
+        if (isset($data['username']) || isset($data['role'])) {
+            $controller->update($id, $data);
+        }
+
+        // update password
+        if (isset($data['password'])) {
+            $controller->updatePassword($id, $data);
+        }
+
+        jsonResponse(["message" => "Data user berhasil diperbarui"]);
         break;
- 
+
     case "DELETE":
         $controller->destroy($id);
         break;
- 
+
     default:
         jsonResponse(["message" => "Method tidak diizinkan"], 405);
 }
--- user/tagihan.php
+++ user/tagihan.php
@@ -3,19 +3,27 @@ require_once "../auth/middleware.php";
 require_once "../config/database.php";
 require_once "../models/UKT.php";
 require_once "../helpers/response.php";
- 
-if (!in_array($currentUser['role'], ['mahasiswa','admin','petugas'])) {
+
+$db = (new Database())->connect();
+$ukt = new UKT($db);
+
+if ($currentUser['role'] === 'mahasiswa') {
+    
+    $tagihan = $ukt->getByMahasiswa($currentUser['uid']);
+} elseif (in_array($currentUser['role'], ['admin','petugas'])) {
+    
+    if (!isset($_GET['mahasiswa_id'])) {
+        jsonResponse(["message" => "mahasiswa_id harus disertakan"], 422);
+    }
+
+    $mahasiswa_id = (int)$_GET['mahasiswa_id'];
+    $tagihan = $ukt->getByMahasiswa($mahasiswa_id);
+} else {
     jsonResponse(["message" => "Akses ditolak"], 403);
 }
 
- 
-$db = (new Database())->connect();
-$ukt = new UKT($db);
- 
-$tagihan = $ukt->getByMahasiswa($currentUser['uid']);
- 
 if (!$tagihan) {
     jsonResponse(["message" => "Tagihan UKT tidak ditemukan"], 404);
 }
- 
+
 jsonResponse($tagihan);
